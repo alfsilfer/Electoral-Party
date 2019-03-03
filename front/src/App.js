665959416e1d9aba334836a6a29478493ebff339
Alfonso Silva <alfsilfer@gmail.com>
2019-03-03 21:14:15 +0100

    version2.0

---
+++ front/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Signup from './components/auth/Signup';
+import { Switch, Route } from 'react-router-dom';
+import AuthService from './components/auth/auth-services';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/auth/Login';
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+    this.service = new AuthService();
+  }
+
+  fetchUser(){
+    if( this.state.loggedInUser === null ){
+      this.service.loggedin()
+      .then(response =>{
+        this.setState({
+          loggedInUser:  response
+        }) 
+      })
+      .catch( err =>{
+        this.setState({
+          loggedInUser:  false
+        }) 
+      })
+    }
+  }
+
+
+  getTheUser= (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+  render() {
+    this.fetchUser()
+    if(this.state.loggedInUser){
+      return (
+        <div className="App">
+          <Navbar userInSession={this.state.loggedInUser} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Navbar userInSession={this.state.loggedInUser} />
+          <Switch>
+            <Route exact path='/signup' render={() => <Signup getUser={this.getTheUser}/>}/>
+            <Route exact path='/' render={() => <Login getUser={this.getTheUser}/>}/>
+          </Switch>
+        </div>
+      );
+    }
+  }
+}
+
+
+export default App;
